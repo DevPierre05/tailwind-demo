729e7b520cc970847b7a708383c20bab38b1dc44
peter <pierrosei2009@yahoo.co.uk>
2024-10-14 16:52:26 +0100

    initial commit

---
+++ src/components/Hero.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="hero-text">
+        <h1>Welcome to Ginza Eats</h1>
+        <p>Tasty snacks anytime, anywhere you desire</p>
+      </div>
+
+      <div className="hero-image">
+        <img
+          src="https://images.unsplash.com/photo-1481464227355-4ede93be2f69?q=80&w=2030&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="Hero"
+          className=""
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
